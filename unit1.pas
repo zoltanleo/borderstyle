9ac3a7bcf83aeecd453ed9df6dc3c85693a70269
ZoltanLeo <leobronstain@gmail.com>
2022-10-31 14:37:27 +0300

    1

---
+++ unit1.pas
@@ -0,0 +1,69 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    CheckGroup1: TCheckGroup;
+    procedure CheckGroup1Click(Sender: TObject);
+    procedure CheckGroup1ItemClick(Sender: TObject; Index: integer);
+    procedure FormShow(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.CheckGroup1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.CheckGroup1ItemClick(Sender: TObject; Index: integer);
+begin
+  if CheckGroup1.Checked[Index]
+  then
+    case Index of
+      0: Self.BorderIcons:= Self.BorderIcons + [biHelp];
+      1: Self.BorderIcons:= Self.BorderIcons + [biSystemMenu];
+      2: Self.BorderIcons:= Self.BorderIcons + [biMinimize];
+      else
+        Self.BorderIcons:= Self.BorderIcons + [biMaximize];
+    end
+  else
+    case Index of
+      0: Self.BorderIcons:= Self.BorderIcons - [biHelp];
+      1: Self.BorderIcons:= Self.BorderIcons - [biSystemMenu];
+      2: Self.BorderIcons:= Self.BorderIcons - [biMinimize];
+      else
+        Self.BorderIcons:= Self.BorderIcons - [biMaximize];
+    end;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+var
+  i: PtrInt = 0;
+begin
+  for i:= 0 to Pred(CheckGroup1.Items.Count) do
+    CheckGroup1ItemClick(Sender,i);
+end;
+
+end.
+
